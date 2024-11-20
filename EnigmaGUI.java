9cb481d3f3895ea191fec444f283a3110c00177e
zoebogan <zoe.bogan@gwu.edu>
2024-11-19 20:09:38 -0500

    EnigmaGUI main method

---
+++ EnigmaGUI.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+
+public class EnigmaGUI {
+    
+    public static void main(String args[]) {
+        EnigmaFrame f = new EnigmaFrame();
+        f.setVisible(true);
+    }
+}
